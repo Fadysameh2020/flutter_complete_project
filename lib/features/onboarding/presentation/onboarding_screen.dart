3bc506a69248218d78d24dfbdfa81089ac40b8a4
Fadysameh2020 <fadysameh10@gmail.com>
2025-04-06 17:41:03 +0200

    ADD: added design onboarding screen

--- lib/features/onboarding/presentation/onboarding_screen.dart
+++ lib/features/onboarding/presentation/onboarding_screen.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_project/core/helpers/extensions.dart';
+import 'package:flutter_complete_project/core/routing/routes.dart';
+import 'package:flutter_complete_project/core/shared_widgets/custom_elevated_button.dart';
+import 'package:flutter_complete_project/core/theming/styles.dart';
+import 'package:flutter_complete_project/features/onboarding/presentation/widgets/logo_and_name.dart';
+import 'package:flutter_complete_project/features/onboarding/presentation/widgets/main_image_and_text.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
@@ -6,8 +13,38 @@ class OnboardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Onboarding Screen'),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 30.h),
+            child: Column(
+              children: [
+                const LogoAndName(),
+                SizedBox(height: 30.h),
+                const MainImageAndText(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30.w),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Manage and schedule all of your medical appointments easily with Docdoc to get a new experience.',
+                        style: TextStyles.font12GrayRegular,
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 30.h),
+                      CustomElevatedButton(
+                        buttonText: 'Get Started',
+                        onPressed: () {
+                          context.pushNamed(Routes.loginScreen);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
